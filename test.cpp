21be9a5279ac60d5e31a5105ce5f10602dad9d6c
shohkoba <78463372+shohkoba@users.noreply.github.com>
2023-04-30 22:32:29 +0900

    引数の修正

--- test.cpp
+++ test.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
     point target_coordinates[] = {{-0.01, 0.0}, {3.0, 0.0}, {3.0, 3.0}, {6.0, 3.0}, {6.0, -3.0}};
-    PurePursuit pp(target_coordinates, 4, 0.5);
+    PurePursuit pp(target_coordinates, 5, 0.5);
 
     while (true)
     {
@@ -18,14 +18,18 @@ int main()
 
     cout << pp.calc_angle({0.0, 0.0}, {1.0, 0.0}, {0.0, 1.0}) << endl;
     cout << pp.calc_angle({0.0, 0.0}, {1.0, 0.0}, {1.0, 1.0}) << endl;
+    cout << pp.calc_angle({6.0, -2.95}, {6.0, 3.0}, {6.0, -3.0}) << endl;
+    cout << endl;
 
-    cout << pp.is_inside(0, {0.1, 0.1}) << endl;
-    cout << pp.is_inside(0, {-0.1, -0.1}) << endl;
+    cout << pp.is_inside({6, -2.95}, 3) << endl;
+    cout << pp.is_inside({-0.1, -0.1}, 0) << endl;
+    cout << endl;
 
     point intersection;
-    cout << pp.perpendicular_dist({0.1, 0.1}, 0, intersection) << endl;
+    cout << pp.perpendicular_dist({6, -2.95}, 3, intersection) << endl;
     cout << intersection.x << " " << intersection.y << endl;
     cout << pp.perpendicular_dist({-0.1, -0.1}, 0, intersection) << endl;
     cout << intersection.x << " " << intersection.y << endl;
+
     return 0;
 }
